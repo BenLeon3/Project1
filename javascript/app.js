bcfb7543458e3f3e1f33fb36a08659dcc73b4905
Benjamin Leon <benleon3@gmail.com>
2018-07-18 21:10:53 -0700

    adding

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,3 @@
-console.log("Hello-world");
-
 $(document).ready(function(){
     $('.single-item').slick({
         dots: true,
@@ -21,14 +19,19 @@ function displayMovieImg() {
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-        // Limits results to only 5 backdrop images
+        // Limits results to only 5 backdrop images. does not support custom per page limits, standard list size is 20.
         for (var i=0; i<5; i++) {
             // Variable pulls the 5 backdrop images
             var backdropImage = response.results[i].backdrop_path;
             carouselSlide = $(".carousel-image-" + (i+1) );
             carouselSlide.attr('src', imageBaseURL + backdropImage);
+            // GIves value of riginal_title the images in the slider
             carouselSlide.attr('data-name', response.results[i].original_title);
-
+            
+            var movieID = response.results[i].id;
+            var movieYouTubeID = "https://api.themoviedb.org/3/movie/" + movieID + "/videos?api_key=0ff882446ecc7061a134cf692047205b&language=en-US";
+            
+            
             carouselSlide.on('click', function() {
                 window.location.href = 'newindex.html?name=' + $(this).attr('data-name');
             })
@@ -39,86 +42,25 @@ function displayMovieImg() {
                 url: queryURL,
                 method: "GET"
             }).then(function(response) {
+                // Limits results to only top 4 news images
+                for (var i=0; i<4; i++){
+                    var newsURL = response.articles[i].url;
+                    var newsIMG = response.articles[i].urlToImage;
+                    var newsTitle = response.articles[i].title;
+                    var newsDescription = response.articles[i].description;
+                    
+                    newsURLCards = $(".news-url-" + (i+1) );
+                    newsURLCards.attr('href', newsURL);
 
-                // for (var i=0; i<4; i++){
-                //     var newsURL = response.articles[i].url;
-                //     var newsIMG = response.articles[i].urlToImage;
-                //     var newsTitle = response.articles[i].title;
-                //     var newsDescription = response.articles[i].description;
+                    newsIMGCards = $(".news-img-" + (i+1) );
+                    newsIMGCards.attr('src', newsIMG);
+            
+                    newsTitleCards = $(".news-title-" + (i+1) );
+                    newsTitleCards.text(newsTitle);
 
-                //     newsTitle1 = $(".")
-                // }
-                // Variables to pull news article 1's URL, image, title, and descritption.
-                var newsArticleURL1 = response.articles[0].url;
-                var newsImg1 = response.articles[0].urlToImage;
-                var movieNewsTitle1 = response.articles[0].title;
-                var movieNewsDescription1 = response.articles[0].description;
-                // Adds news article 1's URL location to the links
-                var newsRow1 = $(".news1");
-                newsRow1.attr('href',newsArticleURL1);
-                // Adds news article 1's image to the image div
-                var displayNewsImage1 = $(".first-news-img");
-                displayNewsImage1.attr('src', newsImg1);
-                // Adds new article 1's title to title div
-                var newsArticleTitle1 = $(".first-news-article");
-                newsArticleTitle1.text(movieNewsTitle1);
-                // Adds new article 1's description to preview div
-                var newsArticleDescription1 = $(".first-news-description");
-                newsArticleDescription1.text(movieNewsDescription1);
-
-                // Variables to pull news article 2's URL, image, title, and descritption.
-                var newsArticleURL2 = response.articles[1].url;
-                var newsImg2 = response.articles[1].urlToImage;
-                var movieNewsTitle2 = response.articles[1].title;
-                var movieNewsDescription2 = response.articles[1].description;
-                // Adds news article 2's URL location to the links
-                var newsRow2 = $(".news2");
-                newsRow2.attr('href',newsArticleURL2);
-                // Adds news article 2's image to the image div
-                var displayNewsImage2 = $(".second-news-img");
-                displayNewsImage2.attr('src', newsImg2);
-                // Adds new article 2's title to title div
-                var newsArticleTitle2 = $(".second-news-article");
-                newsArticleTitle2.text(movieNewsTitle2);
-                // Adds new article 2's description to preview div
-                var newsArticleDescription2 = $(".second-news-description");
-                newsArticleDescription2.text(movieNewsDescription2);
-
-                // Variables to pull news article 3's URL, image, title, and descritption.
-                var newsArticleURL3 = response.articles[2].url;
-                var newsImg3 = response.articles[2].urlToImage;
-                var movieNewsTitle3 = response.articles[2].title;
-                var movieNewsDescription3 = response.articles[2].description;
-                // Adds news article 3's URL location to the links
-                var newsRow3 = $(".news3");
-                newsRow3.attr('href',newsArticleURL3);
-                // Adds news article 3's image to the image div
-                var displayNewsImage3 = $(".third-news-img");
-                displayNewsImage3.attr('src', newsImg3);
-                // Adds new article 3's title to title div
-                var newsArticleTitle3 = $(".third-news-article");
-                newsArticleTitle3.text(movieNewsTitle3);
-                // Adds new article 3's description to preview div
-                var newsArticleDescription3 = $(".third-news-description");
-                newsArticleDescription3.text(movieNewsDescription3);
-
-                // Variables to pull news article 4's URL, image, title, and descritption.
-                var newsArticleURL4 = response.articles[3].url;
-                var newsImg4 = response.articles[3].urlToImage;
-                var movieNewsTitle4 = response.articles[3].title;
-                var movieNewsDescription4 = response.articles[3].description;
-                // Adds news article 4's URL location to the links
-                var newsRow4 = $(".news4");
-                newsRow4.attr('href',newsArticleURL4);
-                // Adds news article 4's image to the image div
-                var displayNewsImage4 = $(".fourth-news-img");
-                displayNewsImage4.attr('src', newsImg4);
-                // Adds new article 4's title to title div
-                var newsArticleTitle4 = $(".fourth-news-article");
-                newsArticleTitle4.text(movieNewsTitle4);
-                // Adds new article 4's description to preview div
-                var newsArticleDescription4 = $(".fourth-news-description");
-                newsArticleDescription4.text(movieNewsDescription4);
+                    newsDescriptionCards = $(".news-description-" + (i+1) );
+                    newsDescriptionCards.text(newsDescription);
+                }
             })
     });
 
